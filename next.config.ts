5bc79f2401eb2cf7e07ec6cb516a3034ae1f9649
SUMAN SASMAL <sasmalsuman04@gmail.com>
2026-03-27 12:57:26 +0530

    suman

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,23 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        // Match all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Use "*" to allow all, or "http://localhost:8081" for strict security
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
